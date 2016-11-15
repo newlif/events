d15042d3929119447e5400c04469a6aeab951923
andrewfn <andrew@drup.org>
2016-11-15 17:53:20 -0500

    added build and update scripts

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#pull changes from git and build upcoming page
+sudo -u apache -H bash -c "git pull"
+sudo -u apache -H bash -c "writeup upcoming.txt"
+writeup build.txt -o build_4490570.php
+# Update Permissions by running this:
+# chown -R apache /home/plousia/nlife2/git/events
+# See also for github hooks:
+# https://gist.github.com/cowboy/619858
+# https://gist.github.com/phedoreanu/11321236
